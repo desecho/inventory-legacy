0d78f202364d2e6448ef20ba593d3a04fb99ed5a
Desecho <desecho@gmail.com>
2013-11-11 14:52:44 +0400

    changes

--- inventory_project/inventory/admin.py
+++ inventory_project/inventory/admin.py
@@ -1,4 +1,4 @@
-from inventory.models import Item, Box
+from inventory.models import Item, Box, Network
 from django.contrib import admin
 
 
@@ -8,8 +8,14 @@ class ItemAdmin(admin.ModelAdmin):
 
 
 class BoxAdmin(admin.ModelAdmin):
-    list_display = ['name', 'box_type', 'deleted']
+    list_display = ['name', 'box_type', 'network', 'deleted']
     ordering = ['name']
+
+
+class NetworkAdmin(admin.ModelAdmin):
+    ordering = ['name']
+
 
 admin.site.register(Item, ItemAdmin)
 admin.site.register(Box, BoxAdmin)
+admin.site.register(Network, NetworkAdmin)
